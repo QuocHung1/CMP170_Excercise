44b1f0cf345e12b655857f90352dd1409ae7b46a
hung dinh <quochunggame@gmail.com>
2021-10-30 07:47:46 +0700

    finalproject

---
+++ Final project/StudentManagement/StudentManagement/IStudentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using static StudentManagement.SearchStudentViewModel;
+
+
+namespace StudentManagement
+    {
+        public interface IStudentService
+        {
+            IList<Student> SearchStudent(string keyword, string hutechclass);
+            Student LoadStudentById(int id);
+            void UpdateOrCreateStudent(Student student);
+            void DeleteStudentById(int id);
+        }
+
+        public interface ICloseable
+        {
+            event EventHandler CloseRequest;
+        }
+    }
+
